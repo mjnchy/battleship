df720c04dca71cf94842e704cbe43c1771d67ff4
mijan <mijanchowdhury2001@gmail.com>
2023-10-22 13:27:35 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+    devtool: 'eval',
+    devServer: {
+      static: './dist',
+      open: {
+        app: {
+          name: 'google-chrome-stable',
+        }
+      }
+    },
+  },
+}
