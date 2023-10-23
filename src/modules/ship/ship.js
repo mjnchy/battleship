31aa170687e5931140b5e9ddb06fcc44593ed373
mijan <mijanchowdhury2001@gmail.com>
2023-10-23 13:57:06 -0400

    made a few adjustments to ship factory and gameboard

--- src/modules/ship/ship.js
+++ src/modules/ship/ship.js
@@ -1,7 +1,7 @@
 function Ship (length, hitCount = 0, sunk = false) {
   let ship = {length, hitCount, sunk};
   return {
-    hit: function () { return this.isSunk() != true? ++ship.hitCount: null },
+    hit: function () { return this.isSunk() !== true? ++ship.hitCount: null },
     isSunk: function () { return ship.sunk = ship.hitCount === ship.length? true: false; },
   };
 };
