cf1eb08a494dc36dc8a528630e18239621a3cc73
mijan <mijanchowdhury2001@gmail.com>
2023-11-25 11:46:46 -0500

    Interaction with the axis-select button and the ships yet to be placed are complete. The changes were basically some quality of life changes where clicking on certain elements would close the list/deselect the selected ship and so on.

--- src/modules/DOM/interaction.js
+++ src/modules/DOM/interaction.js
@@ -1,70 +1,43 @@
-function toggleAxisMenu (menu, button) { 
-  menu.classList.contains("expanded")? button.blur(): null;
-  menu.classList.toggle("expanded");
-};
+import { deselect, externalMenuCollapse, selectAxis, selectShip, toggleAxisMenu } from "./interaction_import_module";
 
-function externalMenuCollapse (element, button, option1, option2) {
-  if (element != button && element != option1 && element != option2)
-  interactElems.axisList.classList.toggle("expanded");
-};
-
-function selectAxis (element, button, list) {
-  const otherOption = element.nextElementSibling? element.nextElementSibling: element.previousElementSibling;
-  element.classList.add("current");
-  otherOption.classList.remove("current");
-  
-  button.dataset.value = element.dataset.value;
-  button.textContent = element.textContent;
-  list.classList.toggle("expanded");
-};
-
-function drawShip (arr) {
-  arr.forEach(identifier => 
-    document.querySelector(`#player-map>.cell[data-identifier="${identifier}"]`).style.backgroundColor = "red");
-};
-
-function getCordinates (identifier) { return [Math.floor(identifier/10), identifier%10] };
-
-function interact () {
-  const interactElems = Object.freeze({
+function getInteractables () {
+  return Object.freeze({
     playerMap: document.querySelector("#player-map"),
+    playerGrid: [...document.querySelectorAll("#player-map>.cell")],
     axisSelected: document.querySelector("#axis-selected"),
     axisList: document.querySelector("#axis-drop-down-list"),
-    axisOption1: document.querySelector(".axis-drop-down-item"),
-    axisOption2: document.querySelector(".axis-drop-down-item:nth-of-type(2)"),
+    axisOptions: [...document.querySelectorAll(".axis-drop-down-item")],
+    shipsOnDeck: [...document.querySelectorAll(".temp-ship-img")],
   });
+};
+
+function interact () {
+  const interactables = getInteractables();
 
   window.addEventListener("click", e => {
     const target = e.target;
-    if (interactElems.axisList.classList.contains("expanded")) 
-    externalMenuCollapse(target, interactElems.axisSelected, interactElems.axisOption1, interactElems.axisOption2);
+    externalMenuCollapse(target, interactables.axisSelected, ...interactables.axisOptions, interactables.axisList);
+    deselect(target, [interactables.axisSelected, interactables.axisList, ...interactables.axisOptions,
+      ...interactables.shipsOnDeck, ...interactables.playerGrid]);
 
     switch (target) {
-      case interactElems.axisSelected:
-        toggleAxisMenu(interactElems.axisList, interactElems.axisSelected); 
+      case interactables.axisSelected:
+        toggleAxisMenu(interactables.axisList, interactables.axisSelected); 
         break;
 
-      case interactElems.axisOption1:
-        selectAxis(target, interactElems.axisSelected, interactElems.axisList);
+      case interactables.axisOptions[0]:
+        selectAxis(target, interactables.axisSelected, interactables.axisList);
         break;
 
-      case interactElems.axisOption2:
-        selectAxis(target, interactElems.axisSelected, interactElems.axisList);
+      case interactables.axisOptions[1]:
+        selectAxis(target, interactables.axisSelected, interactables.axisList);
         break;
     };
-  });
-  
-  interactElems.playerMap.addEventListener("click", e => console.log(e.target));
+  }); 
 
-  // return {
-  //   placeShip: (cb) => {
-  //     document.querySelector("#player-map").addEventListener("click", e => {
-  //       const target = e.target, cor = getCordinates(target.dataset.identifier);
-  //       drawShip(cb, "carrier", cor, "horizontal");
-  //     });
-  //
-  //   }
-  // }
+  interactables.shipsOnDeck.forEach(ship => {
+    ship.addEventListener("click", selectShip);
+  });
 };
 
 export { interact };
---
+++ src/modules/DOM/interaction_import_module.js
@@ -0,0 +1,32 @@
+function toggleAxisMenu (menu, button) { 
+  menu.classList.contains("expanded")? button.blur(): null;
+  menu.classList.toggle("expanded");
+};
+
+function externalMenuCollapse (element, button, option1, option2, list) {
+  if (!list.classList.contains("expanded")) return;
+  if (element != button && element != option1 && element != option2)
+  list.classList.toggle("expanded");
+};
+
+function selectAxis (element, button, list) {
+  const otherOption = element.nextElementSibling? element.nextElementSibling: element.previousElementSibling;
+  element.classList.add("current");
+  otherOption.classList.remove("current");
+  
+  button.dataset.value = element.dataset.value;
+  button.textContent = element.textContent;
+  list.classList.toggle("expanded");
+};
+
+function selectShip (e) {
+  document.querySelectorAll(".temp-ship-img.selected").forEach(selected => selected.classList.remove("selected"));
+  this.classList.add("selected");
+};
+
+function deselect (target, exceptions) {
+  const exceptionMatched = exceptions.some(exception => exception == target);
+  exceptionMatched == true? null: document.querySelector(".selected").classList.remove("selected");
+};
+
+export { toggleAxisMenu, externalMenuCollapse, selectAxis, selectShip, deselect }
