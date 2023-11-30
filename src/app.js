51c333e67bfcf6ac291597def933afeb95bce547
mijan <mijanchowdhury2001@gmail.com>
2023-11-30 18:16:52 -0500

    Recovering from git corruption again

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import "./modules/styles/styles.js";
 import { Player } from "./modules/player/player.js";
-import { shipParameters, drawBoards, setupPrompt, interact } from "./modules/DOM/dom.js";
+import { shipParameters, drawBoards, toggleSetupPrompt, interact, interactables, setupStatBoards } from "./modules/DOM/dom.js";
 
 window.onload = () => {
   const player1 = Player("player1");
   const player2 = Player("player2");
    
   drawBoards([[player1.map.grid, "player-map"], [player1.enemyMap.grid, "enemy-map"]]);
-  setupPrompt();
+  toggleSetupPrompt();
   interact();
+
+  function place () {
+    Object.keys(shipParameters).forEach(ship => {
+      player1.placeShip(shipParameters[ship]);
+      const element = document.querySelector(`.temp-ship-img[data-name="${ship}"]`);
+      element.parentElement.removeChild(element);
+    });
+    toggleSetupPrompt();
+    setupStatBoards();
+  }; 
+
+  interactables.shipPlacer.addEventListener("click", place);
 };
