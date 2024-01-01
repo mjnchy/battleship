85a8527abad1f29d3c9ccd8178d775ff2b5a15e8
mijan <mijanchowdhury2001@gmail.com>
2024-01-01 15:54:40 -0500

    Added the styles for the result display board.

--- src/modules/gameai/gameai.js
+++ src/modules/gameai/gameai.js
@@ -6,7 +6,7 @@ function genRandIdentifier () {
 
 function genRandAxis () {
   const axis = [ "horizontal", "vertical" ];
-  return axis[Math.round(Math.random())];
+  return axis[Math.floor(Math.random() * 2)];
 };
 
 function checkCorForShip (map, arr) {
@@ -33,48 +33,5 @@ function placeShipsOnEnemyBoard (map, cb) {
 function getValidAttackParameters (identifier = genRandIdentifier()) {
   return { identifier, cor: getCor(identifier)};
 };
-
-function attackPlayer () {
-  setTimeout(() => {
-    let attackParams = getValidAttackParameters();
-    while (player1.isAttacked(...attackParams.cor)) attackParams = getValidAttackParameters();
-
-    const target = document.querySelector(`#player-map>.cell[data-identifier="${attackParams.identifier}"]`);
-    let shipHit = player2.attack(player1, ...attackParams.cor);
-    updateAttackedCor("computer", target, attackParams.identifier, player1.map.grid[attackParams.cor[0]][attackParams.cor[1]].ship != null);
-    
-    if (shipHit) {
-      if (!player1.allShipsSunk()) return followUpAttack(attackParams, 1500);
-      return endGame("lost");
-    };
-    switchPlayer();
-  }, delay);
-};
-
-function followUpAttack (player, enemy, prevAtkParams) {
-};
-
-function getFollowUpParams (prevAtkParams) {
-  let operations = [
-    identifier => identifier + 1,
-    identifier => identifier -1,
-    identifier => identifier + 10,
-    identifier => identifier - 10,
-  ],
-  
-  numberOfOperations = 3,
-  getRandOperation = () => Math.round(Math.random() * numberOfOperations),
-  operation = getRandOperation(),
-  newAttackParams = getValidAttackParameters(operations[operation](prevAtkParams.identifier));
-
-  while (enemy.isAttacked(...newAttackParams.cor)) {
-    if (!operations.length) return getValidAttackParameters();
-    operations.splice(operation, 1);
-    numberOfOperations--;  
-    operation = getRandOperation();
-    newAttackParams = getValidAttackParameters(operations[operation](prevAtkParams.identifier));
-  }
-};
-// console.log(followUpAttack(null, null, { identifier: 42 }));
 
 export { placeShipsOnEnemyBoard, getValidAttackParameters };
--- src/modules/player/player.js
+++ src/modules/player/player.js
@@ -14,47 +14,4 @@ function Player (name) {
   };
 };
 
-function Gameboard () {
-  const map = createBoard(), enemyMap = createBoard(), ships = [];
-  map.failed = [];
-  enemyMap.success = [];
-
-  return {
-    map, enemyMap, ships,
-    isAttacked: (x, y) => map.attacked[map.grid[x][y].identifier],    
-    allShipsSunk: () => ships.every(ship => ship.isSunk()),
-
-    placeShip: (arg) => {
-      const newShip = Ship(arg.ship);
-      arg.arr.forEach(cor => map.grid[cor[0]][cor[1]].ship = newShip);
-      ships.push(newShip);
-    },
-
-    receiveAttack: (x, y) => {
-      let hit = false
-      const target = map.grid[x][y];
-      map.attacked[target.identifier] = true;
-      map.totalAtks++;
-      if (!target.ship) map.failed.push([x, y])
-      else { target.ship.hit(); hit = true; };
-
-      return hit;
-    },
-  };
-};
-
-function Ship (name, length = shipLength) {
-  let ship = {
-    name,
-    length: length[name],
-    hitCount: 0,
-    sunk: false,
-  };
-
-  return { ...ship,
-    hit: () => ship.hitCount < ship.length? ++ship.hitCount: null,
-    isSunk: () => ship.hitCount == ship.length? ship.sunk = true: false,
-  };
-};
-
 export { Player };
